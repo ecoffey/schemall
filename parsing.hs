488d5bbb4ad83036e873a95f3bc4f585c8eaa6d5
Eoin Coffey <ecoffey@gmail.com>
2014-02-27 13:19:59 -0700

    simple parsing

---
+++ parsing.hs
@@ -0,0 +1,17 @@
+module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found val"
+
+main :: IO ()
+main = do
+  args <- getArgs
+  putStrLn (readExpr (args !! 0))
+
