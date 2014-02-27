1244ba0d59d9bb8674206965fc49319787837c76
Eoin Coffey <ecoffey@gmail.com>
2014-02-27 16:28:16 -0700

    parse characters

--- parsing.hs
+++ parsing.hs
@@ -11,6 +11,7 @@ data LispVal =
   | DottedList [LispVal] LispVal
   | Number Integer
   | String String
+  | Character Char
   | Bool Bool
 
 spaces :: Parser ()
@@ -32,6 +33,18 @@ parseString = do
   x <- many (parseStringEscapes)
   char '"'
   return $ String x
+
+parseCharacter :: Parser LispVal
+parseCharacter = do
+  string "#\\"
+  char <- string "space" <|> string "newline" <|> many1 anyChar
+  trailing <- optionMaybe (oneOf "( ")
+  case char of
+    "space" -> return $ Character ' '
+    "newline" -> return $ Character '\n'
+    _ -> case trailing of
+      Just _ -> return $ Character (char !! 0)
+
 
 parseAtom :: Parser LispVal
 parseAtom = do
@@ -66,7 +79,7 @@ parseNumber = do
 
 parseExpr :: Parser LispVal
 parseExpr =
-  parseAtom <|> parseString <|> parseNumber
+  parseAtom <|> parseString <|> parseNumber <|> parseCharacter
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
