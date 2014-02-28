9517393430945ffa43d9a01d2d0fd3f6d796ef28
Eoin Coffey <ecoffey@gmail.com>
2014-02-27 19:13:50 -0700

    recursive parsing up in this

--- parsing.hs
+++ parsing.hs
@@ -5,6 +5,15 @@ import Control.Monad
 import Numeric (readOct, readDec, readHex, readInt)
 import Data.Char (digitToInt)
 
+isBinDigit :: Char -> Bool
+isBinDigit c = (c == '0' || c == '1')
+
+readBin :: (Integral a) => ReadS a
+readBin = readInt 2 isBinDigit digitToInt
+
+binDigit :: Parser Char
+binDigit = oneOf "01"
+
 data LispVal =
   Atom String
   | List [LispVal]
@@ -38,12 +47,13 @@ parseCharacter :: Parser LispVal
 parseCharacter = do
   string "#\\"
   char <- string "space" <|> string "newline" <|> many1 anyChar
-  trailing <- optionMaybe (oneOf "( ")
+ -- trailing <- optionMaybe (oneOf "( ")
   case char of
     "space" -> return $ Character ' '
     "newline" -> return $ Character '\n'
-    _ -> case trailing of
-      Just _ -> return $ Character (char !! 0)
+    _ -> return $ Character (char !! 0)
+--    _ -> case trailing of
+--      Just _ -> return $ Character (char !! 0)
 
 
 parseAtom :: Parser LispVal
@@ -56,20 +66,11 @@ parseAtom = do
     "#f" -> Bool False
     _    -> Atom atom
 
-isBinDigit :: Char -> Bool
-isBinDigit c = (c == '0' || c == '1')
-
-readBin :: (Integral a) => ReadS a
-readBin = readInt 2 isBinDigit digitToInt
-
-binDigit :: Parser Char
-binDigit = oneOf "01"
-
 parseNumber :: Parser LispVal
 --parseNumber = liftM (Number . read) $ many1 digit
 --parseNumber = (many1 digit) >>= \p -> return (Number (read p))
 parseNumber = do
-  prefix <- (string "#b") <|> (string "#o") <|> (string "#d") <|> (string "#x") <|> (many1 digit)
+  prefix <- (string "#b") <|> (string "#o") <|> (string "#d") <|> (string "#x") <|> (many1 digit) 
   case prefix of
     "#b" -> liftM (Number . fst . head . readBin) $ many1 binDigit
     "#o" -> liftM (Number . fst . head . readOct) $ many1 octDigit
@@ -77,9 +78,32 @@ parseNumber = do
     "#d" -> liftM (Number . read) $ many1 digit
     _    -> return (Number (read prefix))
 
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
+
 parseExpr :: Parser LispVal
 parseExpr =
-  parseAtom <|> parseString <|> parseNumber <|> parseCharacter
+  parseCharacter
+  <|> parseAtom
+  <|> parseString
+  <|> parseNumber
+  <|> do 
+        char '('
+        x <- try parseList <|> parseDottedList
+        char ')'
+        return x
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
